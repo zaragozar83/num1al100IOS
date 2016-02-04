57622e8a6baa8613fef8337fb4b1952c41c29bd0
zaragozar83 <zaragozar83@gmail.com>
2016-02-03 20:40:06 -0600

    Modificaciones para el juego de memoria

--- juegoMemoria.playground/Contents.swift
+++ juegoMemoria.playground/Contents.swift
@@ -1,5 +1,35 @@
 //: Playground - noun: a place where people can play
+/**
+ ** @autor: Ricardo Zaragoza Solis
+ ** @fecha: 03/02/2016
+ ** @version: 1.0.0
+ **/
 
 import UIKit
 
-var str = "Hello, playground"
+//Generamos el rango del 0 al 100
+let numeros = 0...100
+
+//Iteramos los numeros guardados dentro de nuestra constante numeros
+for n in numeros{
+    
+    //Regla Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!
+    if(n % 5 == 0){
+        print("\(n)Bingo!!!")
+    }
+    
+    //Regla Si el número es par, imprime: # el número + “par!!!
+    if(n % 2 == 0){
+        print("\(n)par!!!")
+    }else if(n % 2 != 0){ //Regla Si el número es impar, imprime: # el número + “impar!!!
+        print("\(n)impar!!!")
+    }
+    //Regla Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!
+    if(n >= 30 && n <= 40){
+        print("\(n)Viva Swift!!!")
+    }
+    
+}
+
+
+
